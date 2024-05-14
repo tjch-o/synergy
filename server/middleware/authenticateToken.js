a4122cee08ab235e2d6b05e8d8e97365ebc97135
tjch-o <e0929841@u.nus.edu>
2024-05-15 02:15:01 +0800

    Initial commit

---
+++ server/middleware/authenticateToken.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const jwt = require("jsonwebtoken");
+
+const authenticateToken = (req, res, next) => {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+        return res.status(401).json({ error: "Authorisation token required" });
+    }
+
+    const token = authorization.split(" ")[1];
+
+    if (token) {
+        jwt.verify(token, process.env.SECRET, (err, user) => {
+            if (err) {
+                return res.status(403).json("Token is not valid");
+            }
+
+            console.log("Token is valid");
+            req.user = user;
+            next();
+        });
+    } else {
+        return res.status(403).json({ error: "No token provided" });
+    }
+};
+
+module.exports = authenticateToken;
---
+++ server/server.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const cors = require("cors");
+const database = require("./database");
+const express = require("express");
+const authenticateToken = require("./middleware/authenticateToken");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = 5000;
+
+const signupRoute = require("./routes/signupRoute");
+const loginRoute = require("./routes/loginRoute");
+
+app.use(signupRoute);
+app.use(loginRoute);
+
+app.get("/", authenticateToken, (req, res) => {
+    res.status(200).send("you have access to the protected route")
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
