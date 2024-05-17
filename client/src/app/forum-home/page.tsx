0d2458cc337eca3bcf6078be4e45613ef39aad22
choo tze jie <119966583+tjch-o@users.noreply.github.com>
2024-05-18 03:25:38 +0800

    Update frontend for forum home page

--- client/src/app/forum-home/page.tsx
+++ client/src/app/forum-home/page.tsx
@@ -22,10 +22,51 @@ const ForumHomePage = () => {
     //     console.log(error)
     // }
 
+    const [posts, setPosts] = useState([]);
+    const username = window.sessionStorage.getItem("username");
+    const router = useRouter();
+
+    useEffect(() => {
+        const fetchPostsData = async () => {
+            const res = await axios.get("http://localhost:5000/forum-posts");
+            setPosts(res.data.posts);
+            fetchPostsData();
+        };
+
+        fetchPostsData();
+    }, []);
+
     return (
-        <p>
-            you made it! welcome {window.sessionStorage.getItem("username")} :D
-        </p>
+        // <p>
+        //     you made it! welcome {window.sessionStorage.getItem("username")} :D
+        // </p>
+        <div className="bg-fixed bg-center bg-cover h-screen">
+            <div className="fixed inset-0">
+                <Image src="/bg.jpg" alt="bg" layout="fill" objectFit="cover" />
+            </div>
+            <div className="relative">
+                <NavBar username={username} />
+                <h1 className="text-3xl text-center m-8 text-white">
+                    Discussion Posts
+                </h1>
+                <div className="grid grids-col-1 md:grids-cols-2 lg:grid-cols-4 gap-2 p-4 justify-items-stretch">
+                    {posts.map((post, index) => {
+                        return (
+                            <Post
+                                key={index}
+                                title={post.title}
+                                content={post.content}
+                                time={post.time}
+                                username={post.username}
+                                likeCount={post.likeCount}
+                                commentCount={post.commentCount}
+                                postId={post.postId}
+                            />
+                        );
+                    })}
+                </div>
+            </div>
+        </div>
     );
 };
 
--- client/src/components/nav/NavBar.tsx
+++ client/src/components/nav/NavBar.tsx
@@ -1,10 +1,17 @@
 import NavBarLogo from "../logos/NavBarLogo";
 
-const NavBar = () => {
+interface NavBarProps {
+    username: string;
+}
+
+const NavBar = ({ username }: NavBarProps) => {
     return (
-        <nav className="flex items-center justify-between flex-wrap opacity-75 bg-black p-6">
+        <nav className="flex items-center justify-between opacity-75 bg-black p-6">
             <div className="flex items-center flex-shrink-0 text-white mr-6">
                 <NavBarLogo />
+            </div>
+            <div className="text-white text-lg">
+                <p> welcome {username} </p>
             </div>
         </nav>
     );
